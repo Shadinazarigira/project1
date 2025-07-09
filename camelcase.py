7e7e93020d8c2f308aa001e8dc56f03aa1740233
Shadi Nazari <shadinazari.giramarketing@gmail.com>
2025-07-09 13:15:59 +0330

    simple_V1

---
+++ camelcase.py
@@ -0,0 +1,33 @@
+# m:int
+# #print(type(m))
+# n= "Shadi Nazari"
+# print(n)
+# print(n.upper())
+# print(n.lower())
+# n=input()
+# print(n)
+# print(n.isnumeric())
+######################################################
+# a=input("enter a model")
+# b=input("enter a label")
+#
+# if a.startswith("s"):
+#     print(a)
+# else:
+#     print("define a new name:")
+#
+# if b.endswith("s"):
+#     input(print("define a new name:"))
+# else:
+#     print(b)
+##########################################################
+# name="majid1majid"
+# print(name[:2])
+# print(name[2:])
+# print(name[2:8:2])
+###########################################################
+# for char in "Shadi":
+#     print(char)
+# for i in range(1,10):
+#     print(i)
+###########################################################
